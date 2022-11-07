5d5c8368fe4b5f24a0fad2cc0df6be6396747498
Andrew Dobson <tandrewlopez@gmail.com>
2022-11-07 13:04:26 -0500

    new logic for netlify forms

--- src/components/pages/Contact/ContactPage.jsx
+++ src/components/pages/Contact/ContactPage.jsx
@@ -2,24 +2,43 @@ import React from "react";
 import "./contact.css";
 
 export const ContactPage = () => {
+  const userName = React.useRef();
+  const userEmail = React.useRef();
+  const userMessage = React.useRef();
+
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  };
+  const handleSubmit = (e) => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact-me" }),
+    });
+  };
+
   return (
     <div className="contactPage">
       <h1>Contact Me</h1>
-      <form
-        name="contact v1"
-        // onSubmit={"submit"}
-        method="POST"
-        data-netlify={true}
-        netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="form-name" value="contact v1" />
+      <form name="contact-me" method="post">
+        <input type="hidden" name="form-name" value="contact-me" />
 
         <label htmlFor="full-name">Full Name:</label>
-        <input id="full-name" type="text" name="full-name" />
+        <input id="full-name" type="text" name="full-name" ref={userName} />
         <label htmlFor="email">Email:</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" ref={userEmail} />
         <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" cols="30" rows="10"></textarea>
+        <textarea
+          id="message"
+          name="message"
+          cols="30"
+          rows="10"
+          ref={userMessage}
+        ></textarea>
         <button type="submit">Submit</button>
       </form>
 
